2b3425b14afe6f2d4ee3ad67fbd2d6ee0e7ca0c4
SUBHASIS PRATIHARI <subhasis.pratihari@gmail.com>
2017-12-09 12:32:09 +0530

    Create MyCalcMulTest.java

---
+++ src/test/java/com/qaagility/controller/MyCalcMulTest.java
@@ -0,0 +1,20 @@
+package com.qaagility.controller;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import com.qaagility.controller.Calcmul;
+public class MyCalcMulTest{
+  
+  Calcmul calc = new ClacMul();
+  
+  @Test
+  public void testCalc(){
+    int result = calc.multiply(9,9);
+    assertEquals("To-Pass", 81, result);
+  }
+  
+  @Test
+  public void testCalc1(){
+    assertEquals("To-Fail", 90, result);
+  }
+  
+}
