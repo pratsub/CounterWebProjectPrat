9e6318831df6d56abcd75fef1d2202bd75cbbe39
SUBHASIS PRATIHARI <subhasis.pratihari@gmail.com>
2017-12-09 12:12:57 +0530

    Create CalcmulTest.java

---
+++ src/test/java/com/qaagility/controller/CalcmulTest.java
@@ -0,0 +1,21 @@
+package com.qaagility.controller;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class CalcmulTest{
+  
+  public void testSlow(){
+    System.out.println("Slow");
+  }
+  
+  public void testSlower(){
+    System.out.println("Slower");
+ }
+  public void testFast(){
+    System.out.println("Fast");
+  }
+  
+  @Test
+  public void testCalc(){
+    System.out.println("Result", 9, 9);
+  }
+  }
