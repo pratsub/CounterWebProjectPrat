c4dc19d054fc181f58d01bbd2c6d87a8d53425f8
SUBHASIS PRATIHARI <subhasis.pratihari@gmail.com>
2017-12-09 11:32:27 +0530

    Update Calcmul.java

--- src/main/java/com/qaagility/controller/Calcmul.java
+++ src/main/java/com/qaagility/controller/Calcmul.java
@@ -5,7 +5,8 @@ public class  Calcmul {
  public static void main(String args[]) {
  
   Calcmul cal = new Calcmul;
-  cal.multiply(3,5);
+  int i = cal.multiply(3,5);
+  System.out.println("Multiplication output " + i);
  }
  
   private int multiply(int a, int b){
