eec7e486679f46133b964c65495909e6913c9f7b
SUBHASIS PRATIHARI <subhasis.pratihari@gmail.com>
2017-12-09 11:29:44 +0530

    Create Calcmul.java

---
+++ src/main/java/com/qaagility/controller/Calcmul.java
@@ -0,0 +1,12 @@
+package com.qaagility.controller;
+public class  Calcmul {
+
+
+ public static void main(String args[]) {
+  multiply(3,5);
+ }
+ 
+  public int multiply(int a, int b){
+    return  a*b;
+ }
+}
